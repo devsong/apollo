ec437f53e04e5a72485cb38b46bf149efe12ec87
guanzhisong <guanzhisong@gmail.com>
2019-10-25 13:46:18 +0800

    feat:modify build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 # apollo config db info
-apollo_config_db_url=jdbc:mysql://127.0.0.1:3306/ApolloConfigDB?serverTimezone=Asia/Shanghai&characterEncoding=utf8&useUnicode=true&useSSL=false&zeroDateTimeBehavior=convertToNull
-apollo_config_db_username=admin
-apollo_config_db_password=1RNbTI@m$zIE114s
+apollo_config_db_url='jdbc:mysql://127.0.0.1:3306/ApolloConfigDB?serverTimezone=Asia/Shanghai&characterEncoding=utf8&useUnicode=true&useSSL=false&zeroDateTimeBehavior=convertToNull'
+apollo_config_db_username='admin'
+apollo_config_db_password='1RNbTI@m$zIE114s'
 
 # apollo portal db info
-apollo_portal_db_url=jdbc:mysql://127.0.0.1:3306/ApolloPortalDB?serverTimezone=Asia/Shanghai&characterEncoding=utf8&useUnicode=true&useSSL=false&zeroDateTimeBehavior=convertToNull
-apollo_portal_db_username=admin
-apollo_portal_db_password=1RNbTI@m$zIE114s
+apollo_portal_db_url='jdbc:mysql://127.0.0.1:3306/ApolloPortalDB?serverTimezone=Asia/Shanghai&characterEncoding=utf8&useUnicode=true&useSSL=false&zeroDateTimeBehavior=convertToNull'
+apollo_portal_db_username='admin'
+apollo_portal_db_password='1RNbTI@m$zIE114s'
 
 # meta server url, different environments should have different meta server addresses
 dev_meta=http://apollo.example.com
